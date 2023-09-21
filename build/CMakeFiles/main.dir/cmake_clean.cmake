81c7fa776f26f0fc47998c5d0e0da1cb2787f73c
Jadonsw123 <jadon.lee.swearingen@gmail.com>
2023-09-21 09:51:25 -0500

    Started the project and wrote shellsort algorithm

---
+++ build/CMakeFiles/main.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main.dir/shellTest.o"
+  "CMakeFiles/main.dir/shellTest.o.d"
+  "CMakeFiles/main.dir/stuff.o"
+  "CMakeFiles/main.dir/stuff.o.d"
+  "main"
+  "main.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
